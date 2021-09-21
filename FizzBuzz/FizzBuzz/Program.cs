cc6abc6d4352a4cac0a880039d24ec30959e03b3
MasseRaphael <masseraphael76@gmail.com>
2021-09-21 11:57:40 +0200

    Début code pour fonctionnement FizzBuzz

---
+++ FizzBuzz/FizzBuzz/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        public static void Main()
+        {
+            int compte = 1;
+
+            do
+            {
+                if (compte % 3 == 0 && compte % 5 == 0)
+                {
+                    Console.WriteLine(compte + "FizzBuzz");
+                }
+                else if (compte % 5 == 0)
+                {
+                    Console.WriteLine(compte + "Buzz");
+                }
+                else if (compte % 3 ==0)
+                {
+                    Console.WriteLine(compte + "Fizz");
+                }
+                else
+                {
+                   Console.WriteLine(compte); 
+                }
+                compte = compte + 1;
+            } while (compte <= 200);
+        }
+    }
+}
